2e5ddacbe4517bb01ed64a7b98ddf0fa0592f769
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-06-03 12:24:08 -0400

    connects angular, js, css

---
+++ client/public/js/app.js
@@ -0,0 +1,8 @@
+var portfolio = angular.module("Portfolio", []);
+
+portfolio.controller("PortfolioController",["$scope","$http","$location",function($scope, $http, $location){
+
+  $scope.hello = "Yo yo yo";
+
+
+}])
