7e7d3b3f5203b31121a7e2315f81b8d94cd460e2
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-06-03 16:51:21 -0400

    adds urls to images

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -8,12 +8,14 @@ portfolio.controller("PortfolioController",["$scope","$http","$location",functio
   [{
     "name":"RESTaurant",
     "img_url":"images/RESTaurant.jpg",
-    "description":"Lorem Ipsum Ipsum"
+    "description":"Lorem Ipsum Ipsum",
+    "url":"https://sams-ugly-restaurant.herokuapp.com/"
   },
   {
     "name":"Dress Me",
     "img_url":"images/dress-me.jpg",
-    "description":"Lorem Ipsum Ipsum"
+    "description":"Lorem Ipsum Ipsum",
+    "url":"https://ga-dress-me-app.herokuapp.com"
   }];
 
   test = function(){
